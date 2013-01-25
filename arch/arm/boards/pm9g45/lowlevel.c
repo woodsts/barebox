a318c86cc0e7bd7b6fa333f1d75958c96634d758
Sascha Hauer <s.hauer@pengutronix.de>
2013-01-25 10:13:39 +0100

    ARM AT91: switch remaining boards to barebox_arm_entry
    For At91 boards which do not have a SDRAM size detection function
    we create a lowlevel reset function which calls barebox_arm_entry
    manually with the known memory size.
    
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

---
+++ arch/arm/boards/pm9g45/lowlevel.c
@@ -0,0 +1,11 @@
+#include <common.h>
+#include <sizes.h>
+#include <asm/barebox-arm-head.h>
+#include <asm/barebox-arm.h>
+#include <mach/hardware.h>
+
+void __naked reset(void)
+{
+	common_reset();
+	barebox_arm_entry(AT91_SDRAM_BASE, SZ_128M, 0);
+}
